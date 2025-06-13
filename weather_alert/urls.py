f101f7e7731fd7780b6f09e6d1762f3cebb27ce9
Dêvid Teófilo <nnick4673@gmail.com>
2025-06-13 15:41:01 -0300

    fix: fix import api module in urls.py file

--- weather_alert/urls.py
+++ weather_alert/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from apps.api.app import api
+from .api.app import api
 from django.contrib import admin
 from django.urls import path
 
